8ddedafb269b7634fa7b016478cf84c496aca9be
Kayla0402 <912454063@qq.com>
2020-03-11 18:29:17 +0800

    codewhy完整的电商项目，教齐全，别人的

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "ignore"
+};
---
+++ src/network/request.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import qs from "qs";
+import { Toast } from "vant";
+import Loading from "../store/index";
+
+const url = "http://123.207.32.32:8000/api/wh";
+
+let config = {
+  baseURL: url
+};
+
+const _axios = axios.create(config);
+
+// 请求拦截
+_axios.interceptors.request.use(
+  req => {
+    // 当getters里面的isLoading为true再显示请求加载
+    if (Loading.getters.isLoading) {
+      Toast.loading({
+        forbidClick: true,
+        message: "加载中..."
+      });
+    }
+    return req;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+// 响应拦截
+_axios.interceptors.response.use(
+  res => {
+    if (res.status === 200 || res.status === 201) {
+      Toast.clear();
+      return res.data;
+    }
+  },
+  err => {
+    Toast.clear();
+    return Promise.reject(err);
+  }
+);
+
+// 全局注册axios qs
+window.axios = _axios;
+window.qs = qs;
